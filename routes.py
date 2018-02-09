05dfed26fe9755b042c6ba7dd65b53314eef5be4
mdonnolo <mdonnolo@tecSpec.mike>
2018-02-08 21:49:24 -0500

    Whatever

--- routes.py
+++ routes.py
@@ -1,6 +1,8 @@
-from flask import Flask, current_app, request, render_template, url_for
+from flask import Flask, current_app, request, render_template, url_for, jsonify
+from subprocess import call
 import sys
-import sys
+import math
+import time
 import argparse
 import json
 import pymysql
@@ -8,22 +10,44 @@ import xml.etree.ElementTree as ET
 
 app = Flask(__name__, static_url_path='')
 
-#parser = argparse.ArgumentParser()
-#parser.add_argument("--unit_tag")
-#args = parser.parse_args()
-#unit_tag = args.unit_tag
-
-
-#ET.register_namespace('', 'http://www.bradycorp.com/printers/bpl')
-
-def get_xml(filename, unit_tag, unit_type):
-    tree = ET.parse('schema.xml')
+def create_xml_file(data):
+    ET.register_namespace('', 'http://www.bradycorp.com/printers/bpl')
+    serial_number_string = str(data['serial_number'])
+    return_piping_string = str(data['return_piping_type'])
+    supply_piping_string = str(data['supply_piping_type'])
+    air_value = get_air_value(data)
+    address_value = get_address_value(data)
+    tree = ET.parse('test.xml')
     root = tree.getroot()
-    text_node = root[1][0][0][0][0]
-    node_value = unit_tag + ":" + unit_type
-    text_node.set('value', node_value)
-    print(text_node.attrib)
-    tree.write('output.xml')
+    labels = root[1][0]
+    unit_tag = labels[2][1][0]
+    bay_risers = labels[4][1][0]
+    return_pipe = labels[6][1][0]
+    supply_pipe = labels[8][1][0]
+    air = labels[10][1][0]
+    passthrough = labels[12][1][0]
+    address = labels[14][1][0]
+    serial_number = labels[15][0]
+    unit_tag.set('value', data['unit_tag'])
+    bay_risers.set('value', data['bay'])
+    return_pipe.set('value', return_piping_string)
+    supply_pipe.set('value', supply_piping_string)
+    air.set('value', air_value)
+    passthrough.set('value', data["pass_through?"])
+    address.set('value', address_value)
+    serial_number.set('value', serial_number_string)
+    filename = './xml_files/output-{}.xml'.format(serial_number_string)
+    tree.write(filename)
+    return filename
+
+def get_address_value(data):
+    return str(int(data['unit_tag'][5:7]))
+
+def get_air_value(data):
+    if data["air_l_?"] == "L":
+        return "LEFT"
+    else:
+        return "RIGHT"
 
 
 def getConfigFromFile(file):
@@ -48,6 +72,17 @@ def get_unit_tag():
     finally:
         connection.close()
 
+def set_unit_tag_timestamp(serial_number, unit_tag):
+    connection = connect_to_database()
+    try:
+        with connection.cursor() as cursor:
+            sql = "UPDATE `units` SET `serial_number`='{}' WHERE `unit_tag` = '{}'".format(serial_number, unit_tag)
+            print(sql)
+            cursor.execute(sql)
+        connection.commit()
+    finally:
+        connection.close()
+
 def get_units():
     connection = connect_to_database()
     try:
@@ -59,6 +94,11 @@ def get_units():
     finally:
         connection.close()
 
+def format_data(data):
+    if data:
+        data["serial_number"] = math.floor(time.time())
+        return data
+
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
@@ -66,3 +106,15 @@ def index():
 @app.route("/get-unit-tag-list")
 def get_unit_tag_list():
     return get_units()
+
+@app.route("/save-unit-tag-order", methods=["POST"])
+def save_unit_tag_list():
+    data_json = request.get_json()
+    data = format_data(data_json)
+    serial_number = str(data['serial_number'])
+    unit_tag = str(data['unit_tag'])
+    set_unit_tag_timestamp(serial_number, unit_tag)
+    filename = create_xml_file(data)
+    shell_command_to_print = "cat {} | nc 192.168.2.156 9100".format(filename)
+    #call([shell_command_to_print], shell=True)
+    return "THIS WORKED"
--- static/app.js
+++ static/app.js
@@ -4,48 +4,59 @@
 function MainController($http){
 	var vm = this;
 	vm.model = {
-		inputTag: "",
-		unitData: null
+				inputTag: "",
+				unitData: null
 	};
 	vm.setUnitData = setUnitData;
 	vm.getUrlSrc = getUrlSrc;
+	vm.saveOrder = saveOrder;
 
-	activate();
+		activate();
 
-	//////////
+		//////////
 
-	function activate(){
-		$http.get("/get-unit-tag-list")
-		   .then(function(e){
-			vm.model.units = e.data;
-		    });
-	}
-
-	function setUnitData() {
-		var unitData = _.find(vm.model.units, setCorrectUnitData);
-		vm.model.unitData = unitData ? unitData : null;
-
-		function setCorrectUnitData(item) {
-			var inputTag = vm.model.inputTag.replace(/ /, '');
-			return (item.unit_tag.toLowerCase() === inputTag.toLowerCase()) 		
+		function activate(){
+				$http.get("/get-unit-tag-list")
+				.then(getUnitTagList);
 		}
-	}
 
-	function getUrlSrc() {
-		if(vm.model.unitData){
-			var unitData = vm.model.unitData;
-			var unitString = unitData.unit_type.split("-").join("").substr(0,2);
-			var unitAppend = unitData["air_l_?"] === "L" ? unitData["air_l_?"] : unitData["air_r_?"];
-console.log(unitData)
-			return "/img/" + unitString + unitAppend + unitData["pass_through?"] + ".jpg";
-		} else {
-			return null;
+		function getUnitTagList(e){
+				vm.model.units = e.data;
 		}
-	}
 
-}
+		function setUnitData() {
+				var unitData = _.find(vm.model.units, setCorrectUnitData);
+				vm.model.unitData = unitData ? unitData : null;
 
-var app = angular.module("PipeApp", []);
-app.controller("MainController", MainController);
+				function setCorrectUnitData(item) {
+						var inputTag = vm.model.inputTag.replace(/ /, '');
+						return (item.unit_tag.toLowerCase() === inputTag.toLowerCase()) 		
+				}
+		}
+
+		function getUrlSrc() {
+				if(vm.model.unitData){
+						var unitData = vm.model.unitData;
+						var unitString = unitData.unit_type.split("-").join("").substr(0,2);
+						var unitAppend = unitData["air_l_?"] === "L" ? unitData["air_l_?"] : unitData["air_r_?"];
+						return "/img/" + unitString + unitAppend + unitData["pass_through?"] + ".jpg";
+				} else {
+						return null;
+				}
+		}
+
+		function saveOrder() {
+			$http({
+					url:"/save-unit-tag-order" ,
+					data: JSON.stringify(vm.model.unitData),
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+			});
+		}
+
+		}
+
+		var app = angular.module("PipeApp", []);
+		app.controller("MainController", MainController);
 
 })();
